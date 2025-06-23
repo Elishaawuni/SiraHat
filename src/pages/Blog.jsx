28b1b0cc364768c70b1cd884959659b0ff801670
macPRO <macpro@macPROs-MacBook-Pro.local>
2025-06-23 02:44:40 +0000

    feat: update blog images to use provided Google Drive links

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const blogImages = [
+  "https://lh3.googleusercontent.com/d/1lJtNQSWxJIcvDo8Woz9pygUui0YMHFim=w1200",
+  "https://lh3.googleusercontent.com/d/1Gps_5UfVRQTiFPfXUNnqE7YQydgLRj4g=w1200",
+  "https://lh3.googleusercontent.com/d/1A7ArmFRBI9zqaVxDyrCdY8p0hv2ZDe6j=w1200"
+];
+
 export default function Blog() {
   return (
     <section className="p-10">
@@ -9,7 +15,7 @@ export default function Blog() {
         {[...Array(3)].map((_, i) => (
           <Link to={`/blog/${i}`} key={i}>
             <div className="bg-white rounded shadow p-4 hover:shadow-lg">
-              <img src={`/images/blog-${i + 1}.png`} alt="Blog" className="w-full h-32 object-cover mb-4" />
+              <img src={blogImages[i]} alt={`Blog ${i+1}`} className="w-full h-32 object-cover mb-4" />
               <h4 className="text-sm font-semibold mb-2">Crafting Culture: Story {i + 1}</h4>
               <p className="text-xs text-gray-600">Discover how our artisans bring each piece to life using time-honored techniques.</p>
             </div>
